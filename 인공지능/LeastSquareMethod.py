d09e94e3c34ace06a2bc0af60b76fdc221bd3f93
김동균 <nabilera1@naver.com>
2021-10-14 16:12:03 +0900

    1014

---
+++ 인공지능/LeastSquareMethod.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+x = [2, 4, 6, 8]
+y = [81, 93, 91, 97]
+
+# x와 y의 평균값
+mx=np.mean(x)
+my=np.mean(y)
+print('x의 평균값')
--- 제어문 if.py
+++ 제어문 if.py
@@ -13,7 +13,7 @@
 a = int(input())
 if a >= 70:
     print("최우수")
-elif a >= 50:
+elif a >= 50:  # elif ==> else if
     print("우수")
 elif a >= 20:
     print("보통")
