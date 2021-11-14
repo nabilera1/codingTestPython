c0e00163997df84a965fd4c617617f47ca9b2fe9
KIMDONGGYUN <nabilera1@naver.com>
2021-11-14 21:06:01 +0900

    1114

---
+++ 트리 결정트리 AI.py
@@ -0,0 +1,8 @@
+# AI 결정 트리 학습법(decision tree learning)
+# 예측 모델링 방법 중 하나.
+# Yes, No 중 하나를 선택하면서 최종 의사 결정을 하는 것
+# 예) 어떤 조건을 여러 차례 따라가면 병원에 방문해야하는지 등을 안내 받음
+#
+# from sklearn.tree import DecisionTreeClassifier
+# classifier=DecisionTreeClassifier()
+# classifier.fit(학습데이터셋)
