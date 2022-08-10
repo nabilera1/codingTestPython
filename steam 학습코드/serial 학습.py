693f58cb6990d6bd832507b2092dba9b1bf23559
김동균 <nabilera1@naver.com>
2022-08-10 17:30:54 +0900

    0810 serial

---
+++ steam 학습코드/serial 학습.py
@@ -0,0 +1,30 @@
+from configparser import LegacyInterpolation
+from threading import Thread
+import serial #파이참에서 pyserial설치
+import time
+import collections
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import struct
+import pandas as pd
+import numpy as np
+
+plt.ion()
+fig = plt.figure()
+i=0
+x=list()
+y=list()
+i=0
+ser = serial.Serial('COM6',9600)
+ser.close()
+ser.open()
+while True:
+    data = ser.readline()
+    print(data.decode())
+    x.append(i)
+    y.append(data.decode())
+
+    plt.scatter(i, float(data.decode()))
+    i+=1
+    plt.show()
+    plt.pause(0.0001)
