d841981ad2efd75a84bc1b4ca881473006801c54
김동균 <nabilera1@naver.com>
2022-08-24 14:17:12 +0900

    teacherroom0824 update

--- 리스트/리스트.py
+++ 리스트/리스트.py
@@ -1,15 +1,15 @@
 # 팩토리얼 구현
 # 5! = 120
 
-num=int(input("자연수 입력 : "))
-fac=1
+num = int(input("자연수 입력 : "))
+fac = 1
 
-if num<0:
+if num < 0:
     print("1보다 큰 수를 입력해주세요.")
-elif num==0:
+elif num == 0:
     print("0팩토리얼은 1입니다.")
 else:
-    for i in range(1,num+1):
-        fac=fac*i
-    print(num,"팩토리얼은 ",fac,"입니다.")
+    for i in range(1, num + 1):
+        fac = fac * i
+    print(num, "팩토리얼은 ", fac, "입니다.")
     # print(f'{num} 팩토리얼은 {fac}입니다.')
--- 파이썬 수업코드 2022 2학기/들어가기.py
+++ 파이썬 수업코드 2022 2학기/들어가기.py
@@ -2,24 +2,23 @@
 print('Kim Dong Gyun'')
 print("대구과학고등학교")
 print('123*456 =', end=' ')
-print(123*456)
-print('2^500=', 2**500)
-print(f'2^500= {2**500}')
+print(123 * 456)
+print('2^500=', 2 ** 500)
+print(f'2^500= {2 ** 500}')
 #########
 print('a+b=', a + b)
 print(f'a+b= {a * b}')
 print(f'a+b= ', end='')
-print(a+b)
-
+print(a + b)
 
 ##########
-a=4.24E10
+a = 4.24E10
 print(a)
-b=0x8ff
+b = 0x8ff
 print(b)
-c=20
-d=int(str(c),16) #16진수로 저장
-#d=int('20',16)
-print(d) #10진수로 출력
-print(int('20',8))
-print(int('0x8ff',16))
+c = 20
+d = int(str(c), 16)  # 16진수로 저장
+# d=int('20',16)
+print(d)  # 10진수로 출력
+print(int('20', 8))
+print(int('0x8ff', 16))
