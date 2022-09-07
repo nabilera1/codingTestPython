8eab6b43a3b0448c125bf1544fe476ab4548f538
nabilera1 <nabilera1@naver.com>
2022-09-07 16:09:59 +0900

    multiroom1 0907

---
+++ 파이썬 수업코드 2022 2학기/1학년1반.py
@@ -0,0 +1,58 @@
+# num = input('개수를 입력하세요.')
+#
+# # 나는 사과 __개를 먹었다. 로 출력하는 코드를 작성해보시오.
+# print('나는 사과 {0}개를 먹었다.'.format(num))
+# print('나는 사과 %s개를 먹었다.' % (num))
+# print(f'나는 사과 {num}개를 먹었다.')
+# print(f"나는 사과 {num}개를 먹었다.")
+# y = 3.42134234
+# print('{0:0.4f}'.format(y))
+# print('{0:10.4f}'.format(y))
+# print('{0:<10.4f}'.format(y))
+# print('{0:>10.4f}'.format(y))
+
+
+# n = 4.567173
+# print("{0:0.4f}".format(n))
+# print("{:0.4f}".format(n))
+# print(f"{n:0.4f}")
+# print(f"{n}")
+
+# a, b = map(int, input().split())
+# a = int(input())
+# b = int(input())
+# print(f"{a:>10}")
+# print(f"*{b:>9}")
+# print("-" * 10)
+# print(f"{a * b:>10}")
+# a = int(input())
+# b = int(input())
+# a, b = map(int ,input().split())
+# print("{0:>7}".format(a))
+# print('x', end='')
+# print("{0:>6}".format(b))
+# print("{0:<7}".format('-------'))
+# print('-' * 10)
+# print("{0:>7}".format(a * b))
+
+# a = '개울가에 소년이 나타났다. 소녀는 개울가를 건너고자 한다.'
+# print(a.count('개울가'))
+# print(a.find('소년'))
+# # 소년은 6번째에 나옵니다.
+# print(f'소년은 {a.find("소년")+1}번째에 나옵니다.')
+# print(a.find('소련'))  # -1
+# print(a.index('소년'))
+# print(a.index('소련'))
+#
+# print(','.join("Hello"))
+
+# Yes / No ?  yEs  , YEs, yes, Yes1
+# a = input()
+# print(a.upper())
+# print(a.lower())
+#
+# str = ' Hello '
+# print(str)
+# print(str.strip())
+# a = 'I like bananas. You like bananas.'
+# print(a.replace('banana', 'apple'))
