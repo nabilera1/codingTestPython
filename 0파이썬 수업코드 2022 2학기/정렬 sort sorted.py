adea8b3f78a8b015d2318f4438551137db78f8a5
nabilera4 <nabilera4@ts.hs.kr>
2022-09-07 20:59:46 +0900

    from notebook 0907-1

--- 0파이썬 수업코드 2022 2학기/정렬 sort sorted.py
+++ 0파이썬 수업코드 2022 2학기/정렬 sort sorted.py
@@ -1,19 +1,18 @@
 '''
 list.sort() : list 원본이 바뀜(in-place), 리턴은 None
 sorted(list) : list 원본은 그대로 두고 정렬된 리스트 값을 리턴
-
 '''
 
 nums = [1, 4, 3, 2]
 print('1:', nums)
-newNums = nums.sort()  # 원본을 정렬하고 수정합니다(in-place)
+newNums = nums.sort()  # 원본을 정렬하고 수정(in-place)
 print('-----정렬 후-----')
 print('2:', nums)
 print('3:', newNums)
 
 nums1 = [1, 4, 3, 2]
 print('\n1:', nums1)
-newNums1 = sorted(nums1)  # 원본은 유지하고 정렬한 새 리스트를 만듭니다
+newNums1 = sorted(nums1)  # 원본은 유지하고 정렬한 새 리스트에 복사
 print('-----정렬 후-----')
 print('2:', nums1)
 print('3:', newNums1)
@@ -24,14 +23,17 @@ reverse() : 순서 뒤집기
 '''
 nums = [1, 4, 3, 2]
 print('\n1:', nums)
-newNums = nums.sort(reverse=True)  # 원본을 정렬하고 수정합니다(in-place)
+newNums = nums.sort(reverse=True)  # 원본을 정렬하고 수정(in-place)
 print('-----내림 차순 정렬 후-----')
 print('2:', nums)
 print('3:', newNums)
 
 nums = [1, 4, 3, 2]
 print('\n1:', nums)
-newNums = nums.reverse()  # 정렬이 아닌 뒤집기이고 수정합니다(in-place)
+newNums = nums.reverse()  # 정렬이 아닌 뒤집기이고 수정(in-place)
 print('-----reverse 후-----')
 print('2:', nums)
 print('3:', newNums)
+
+
+# https://docs.python.org/ko/3/howto/sorting.html
