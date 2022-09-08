01011aa058c9d0844a89c56606fc01aa6840c563
nabilera1 <nabilera1@naver.com>
2022-09-08 11:01:01 +0900

    multiroom1 0908

---
+++ 파이썬 수업코드 2022 2학기/1학년2반1.py
@@ -0,0 +1,38 @@
+# a = '개울가에 소년이 나타났다. 소녀는 개울가를 건너고자 한다.'
+# print(a.count('개울가'))
+# # print(a.count(' '))
+# # 소년 - > 준규
+# # print(a.replace('소년', '준규'))
+# # a = a.replace('소년', '준규')
+# # print(a)
+# # find     index
+# print(f"소년은 {a.find('소년')+1}번째 있습니다.")
+# print(a.find('철수'))
+# print(a.index('철수'))
+
+# a = ','.join('abcd')
+# print(a)
+# print(type(a))
+# # print(type(''))
+
+# Yes / No ?   Yes  yEs yes YES yes1
+# a = input('Yes / No ?')
+# a = a.upper()  # 대문자  lower() 소문자
+# print(a)
+
+#
+# a = 'Life is Good'
+# print(a.split())
+# a = 'Life/is/Good'
+# print(a.split('/'))
+
+
+# a = [1, 2, 3, ['a', 'b', 'c']]
+# print(a[-1])
+# print(a[3])
+# print(a[3][0])
+# print(a[-1][0])
+# print(a[-1][-3])
+
+arr = [1, 2, ['a', 'b', ['Life', 'is']]]
+print(a[2][2][0][2])
