78997c713432999072a8d780ad2a12b72f1a2027
nabilera1 <nabilera1@naver.com>
2022-09-14 10:21:22 +0900

    multiroom1 0914

---
+++ 0파이썬 수업코드 2022 2학기-1/1학년5반.py
@@ -0,0 +1,35 @@
+# a = [1, 2, 3, 1, 2, 3]
+# a.pop(7)
+# print(a)
+
+# money=2000
+# card=True
+# if money>=3000 or card:
+#     print('택시')
+# else:
+#     print('조깅')
+
+# print('a' in 'python')
+# print('t' in 'python')
+
+# pocket=['paper', 'cellphone', 'money']
+# if 'money' in pocket:
+#     pass
+# else:
+#     print('조깅')
+
+a = "Life is too short, you need python"
+if "wife" in a: print("wife")
+elif "python" in a and "you" not in a:
+    print("python")
+elif "shirt" not in a:
+    print("shirt")
+elif "need" in a:
+    print("need")
+else:
+    print("none")
+
+
+
+
+
