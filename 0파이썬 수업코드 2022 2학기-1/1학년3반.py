159203c5a5887d222fa3653f716c64b6c02fccc4
nabilera1 <nabilera1@naver.com>
2022-09-16 10:19:36 +0900

    multiroom1 0916

---
+++ 0파이썬 수업코드 2022 2학기-1/1학년3반.py
@@ -0,0 +1,20 @@
+# lst = ['Apple', 'Banana', 'Orange']
+# print(', '.join(f'{x}s' for x in lst))
+
+# a = input()
+# # a.upper()
+# a = a.lower()
+# print(a)
+
+# a = '개울가에 소년이 나타났다. 소녀는 개울가를 건너고자 한다.'
+# a = a.replace(' ', '')
+# print(a)
+# print(len(a))
+
+# a = 1
+# print(type(a))
+# arr = [1, 2, ['a', 'b', ['Life', 'is']]]
+# print(arr[-1])
+# print(arr[2])
+
+print([x**2 for x in [1,2,3]])
