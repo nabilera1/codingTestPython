825d129e7795d8353195acbb1cf28d9c8d105aa5
nabilera1 <nabilera1@naver.com>
2022-09-23 10:20:46 +0900

    multiroom1 0923

--- 0파이썬 수업코드 2022 2학기-1/1학년3반.py
+++ 0파이썬 수업코드 2022 2학기-1/1학년3반.py
@@ -16,5 +16,34 @@
 # arr = [1, 2, ['a', 'b', ['Life', 'is']]]
 # print(arr[-1])
 # print(arr[2])
+# 1에서 1000까지의 3의 배수의 총합은?
+# print(sum([x for x in range(1, 1001) if x % 3 == 0]))
+#
+#
+# t = 1
+# print(type(t))
+# t = (1,)
+# print(type(t))
+# t = (1)
+# print(type(t))
+# t = [1]
+# print(type(t))
 
-# print([x**2 for x in [1,2,3]])
+# d = {}
+# print(type(d))
+# dic = {'name':'pey', 'phone':'0119993323', 'birth': '1118'}
+# print(dic)
+# # 1 는 대구시
+# dic[1] = '대구시'
+# print(dic)
+# print(dic[1])
+
+
+d = {}
+k = input()
+d[k] = input()
+print(d)
+https://docs.python.org/ko/3/library/pprint.html
+
+none None 비교
+
