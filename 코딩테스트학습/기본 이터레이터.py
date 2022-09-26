e5580fa2bebe9b6a3fb0c5ac3f3a26dc8bcd65ab
nabilera1 <nabilera1@naver.com>
2022-09-26 17:01:32 +0900

    multiroom1 0926

--- 코딩테스트학습/기본 이터레이터.py
+++ 코딩테스트학습/기본 이터레이터.py
@@ -1,6 +1,10 @@
 li =[1,2,3,4]
 lii = iter(li)
 print(type(lii)) # <class 'list_iterator'>
+# for i in range(len(li)):
+#     print(lii.__next__(), end=' ')
+# 매직 메소드 사용  __next__
+
 for i in range(len(li)):
-    print(lii.__next__(), end=' ')
+    print(next(lii))   #위의 코드 실행하면 더 이상 동작안됨.
 
