beb3ded690d844ff2ffc570ae8045414b8171342
nabilera1 <nabilera1@naver.com>
2022-09-29 12:23:26 +0900

    multiroom1 0929

--- 0파이썬 수업코드 2022 2학기-1/1학년2반1.py
+++ 0파이썬 수업코드 2022 2학기-1/1학년2반1.py
@@ -65,7 +65,7 @@
 # #1에서 100까지 총합이 출력됨.
 # print(sum(list(range(1, 101))))
 
-#구구단 출력
+# 구구단 출력
 
 # for i in range(2,10):
 #     for j in range(1, 10):
@@ -87,6 +87,74 @@
 # print(type(a)) # <class 'dict'>
 
 
+#
+# s2 = set("Hello")
+# # print(s2)
+#
+# l1 = [1, 2, 3, 4, 5, 6, 3, 4]
+# l2 = [4, 5, 6, 7, 8, 9, 8]
+# s1 = set(l1)
+# s2 = set(l2)
+# print(s1)
+# print(s2)
+# # 교집합, intersection, &
+# print(f'교집합 : {s1.intersection(s2)}')
+# print(f'교집합 : {s1 & s2}')
+# # 합집합, union, |
+#
+# # 차집합, difference, -
 
-s2 = set("Hello")
-print(s2)
+# 집합 자료형의 함수
+# s1 = {1, 2, 3}
+# print(type(s1))
+# s1 = s1 | {4}  # + 은 에러
+# print(s1)
+#
+# s1.add(5)
+# print(s1)
+#
+# s1.update([6, 7, 8])
+# print(s1)
+#
+# s1.discard(7)  # remove, discard로 바꿔 실행해보세요.
+# print(s1)
+# s1.discard(7)
+# print(s1)
+
+# # bool a = True # C++ 스타일, 파이썬은 안됨.
+# # SyntaxError: invalid syntax. Perhaps you forgot a comma?
+# a = True
+# print(a)
+# print(type(a))
+# a = 1
+# print(a)
+# print(type(a))
+# a = [1]
+# print(id(a))
+# a = [1, 2, 3]
+# print(id(a))
+# b = a
+# print(id(b))
+# a, b = 5, 3
+# print(a, b)
+# a, b = b, a
+# print(a, b)
+
+def add_mul(ch, *args):
+    print(type(args))
+    if ch == 'add':
+        result = 0
+        for i in args:
+            result = result + i
+    elif ch == 'mul':
+        result = 1
+        for i in args:
+            result = result * i
+    return result
+
+
+print(add_mul('add', 1, 2, 3))
+print(add_mul('add', 1, 2))
+n = list(map(int, input().split()))
+print(n)
+print(add_mul('add', n[0], n[1]))
