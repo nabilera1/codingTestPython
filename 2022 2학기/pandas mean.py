ee284da3ecc1efc1393e22825e9ddf153058f958
nabilera4 <nabilera4@ts.hs.kr>
2022-10-15 23:05:37 +0900

    notebook 1015

--- 2022 2학기/pandas mean.py
+++ 2022 2학기/pandas mean.py
@@ -1,14 +1,28 @@
 
 import pandas as pd
 
-# dataset
-df = pd.DataFrame([[1, 2, 3], [4, 5, 6],
-                   [7, 8, 9], [10, 11, 12]],
-                  columns=['a', 'b', 'c'])
+# # dataset
+# df = pd.DataFrame([[1, 2, 3], [4, 5, 6],
+#                    [7, 8, 9], [10, 11, 12]],
+#                   columns=['a', 'b', 'c'])
+#
+# # dataFrame
+# print(df)
+#
+# # finding mean by rows
+# # df.mean(axis='rows')
+# df.mean(axis=0)
 
-# dataFrame
-print(df)
+# https://www.geeksforgeeks.org/how-to-use-axis0-and-axis1-in-pandas/
 
-# finding mean by rows
-# df.mean(axis='rows')
-df.mean(axis=0)
+
+# import pandas as pd
+#
+# # importing our dataset
+# df = pd.read_csv('hiring.csv')
+#
+# # dropping the column named 'experience'
+# df = df.drop(['experience'], axis=1)
+#
+# # 'viewing the dataframe
+# df.head()
