09499e00b1f55e4a20c19cbfec732867843c9e06
김동균 <nabilera1@naver.com>
2022-11-07 15:12:52 +0900

    teacher1107 update

---
+++ 코딩테스트학습/파일입출력1.py
@@ -0,0 +1,17 @@
+#자신의 컴퓨터 C:\1  폴더가 있어야 함.
+
+# f = open("C:/1/newfile.txt", 'w')
+# for i in range(1, 11):
+#     data = "%d번째 줄입니다.\n" % i
+#     f.write(data)
+# f.close()
+
+
+f = open("./file/newfile1.txt", "w")
+for i in range(1,11):
+    f.write(f'{i}번째 줄입니다.\n')
+f.close()
+
+#open and read the file after the writing or appending:
+f = open("./file/newfile1.txt", "r")
+print(f.read())
