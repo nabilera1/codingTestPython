bfb0365fc7dfa29d8111c04219a98ea64f9bb6b1
김동균 <nabilera1@naver.com>
2023-04-04 13:33:17 +0900

    0404 update

--- 2023 파이썬 자료구조/LinkedListLIFO_init_1.py
+++ 2023 파이썬 자료구조/LinkedListLIFO_init_1.py
@@ -4,14 +4,11 @@ class Node(object):
         self.pointer = None
         # self.pointer = pointer
 
-
-# class LinkedListLIFO(object):
-class LinkedListLIFO(Node):  # 파일 분할 안할 시 상속으로 처리
+class LinkedListLIFO:
     def __init__(self):
         self.head = None
         self.length = 0
 
-    # 헤드부터 각 노드의 값을 출력
     def _printList(self):
         node = self.head
         while node:
@@ -19,15 +16,13 @@ class LinkedListLIFO(Node):  # 파일 분할 안할 시 상속으로 처리
             node = node.pointer
         print()
 
-    # 이전 노드를 기반으로 노드를 삭제한다.
     def _delete(self, prev, node):
-        self.length = -1
+        self.length -= 1
         if not prev:
-            self.head = node.pointer  # None 아닌가?
+            self.head = None
         else:
             prev.pointer = node.pointer
 
-    # add new Node
     def _add_old(self, value):
         self.length += 1
         self.head = Node(value, self.head)
@@ -51,7 +46,6 @@ class LinkedListLIFO(Node):  # 파일 분할 안할 시 상속으로 처리
 
         return node, prev, i
 
-    # find node by value
     def _find_by_value(self, value):
         prev = None
         node = self.head
@@ -64,7 +58,6 @@ class LinkedListLIFO(Node):  # 파일 분할 안할 시 상속으로 처리
                 node = node.pointer
         return node, prev, found
 
-    # 해당 인덱스의 노드 삭제하기
     def deleteNode(self, index):
         node, prev, i = self._find(index)
         if index == i:
@@ -72,7 +65,6 @@ class LinkedListLIFO(Node):  # 파일 분할 안할 시 상속으로 처리
         else:
             print(f'인덱스 {index}에 해당하는 노드가 없습니다.')
 
-    # 값에 해당하는 노드를 찾아서 삭제한다.
     def deleteNodeByValue(self, value):
         node, prev, found = self._find_by_value(value)
         if found:
